14d2d9fdd2166aea49005c4dca80b7634a65b5dd
allenjamesvinoy14 <allenjames.vinoy2017@vitstudent.ac.in>
2019-09-08 01:45:24 +0530

    heroku deploy v1

--- login/config/db.php
+++ login/config/db.php
@@ -2,7 +2,14 @@
     
     require 'constants.php';
 
-    $conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+
+    $conn = new mysqli($server,$username,$pass,$db);
 
     if($conn->connect_error){
         die("Database Error: ".$conn->connect_error);
