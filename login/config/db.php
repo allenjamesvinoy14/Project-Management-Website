b73e735df8962395ecaa7bc9076585183576a359
allenjamesvinoy14 <allenjames.vinoy2017@vitstudent.ac.in>
2019-09-08 23:59:27 +0530

    clear db variables assigned to db.php

--- login/config/db.php
+++ login/config/db.php
@@ -1,14 +1,14 @@
 <?php 
     require 'constants.php';
 
-    // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    // $server = $url["host"];
-    // $username = $url["user"];
-    // $password = $url["pass"];
-    // $db = substr($url["path"], 1);
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
 
 
-    $conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+    $conn = new mysqli($server,$username,$password,$db);
 
     if($conn->connect_error){
         die("Database Error: ".$conn->connect_error);
