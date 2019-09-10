54c7f2ae124f72997b9b5dbbc4e7e111f59b1d17
allenjamesvinoy14 <allenjames.vinoy2017@vitstudent.ac.in>
2019-09-10 23:53:10 +0530

    Main portal basic design

---
+++ main/index.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>ProJ</title>
+</head>
+<body>
+    <div class="main">
+        <div class="col-md-8 offset-md-2 h-100 row justify-content-center projitems mt-5 mb-5">
+            <h1>Hello</h1>
+        </div>
+        <div class="col-md-8 offset-md-2 h-100 row justify-content-center projitems mt-5 mb-5">
+            <h1>Hello</h1>
+        </div>
+        <div class="col-md-8 offset-md-2 h-100 row justify-content-center projitems mt-5 mb-5">
+            <h1>Hello</h1>
+        </div>
+        <div class="col-md-8 offset-md-2 h-100 row justify-content-center projitems mt-5 mb-5">
+            <h1>Hello</h1>
+        </div>
+        <div class="col-md-8 offset-md-2 h-100 row justify-content-center projitems mt-5 mb-5">
+            <h1>Hello</h1>
+        </div>
+        <div class="col-md-8 offset-md-2 h-100 row justify-content-center projitems mt-5 mb-5">
+            <h1>Hello</h1>
+        </div>
+        <div class="col-md-8 offset-md-2 h-100 row justify-content-center projitems mt-5 mb-5">
+            <h1>Hello</h1>
+        </div>
+        <div class="col-md-8 offset-md-2 h-100 row justify-content-center projitems mt-5 mb-5">
+            <h1>Hello</h1>
+        </div>
+    </div>
+</body>
+</html>
